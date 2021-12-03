335afdfe3003260704fc3a76c22c7d673bdc8dff
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-03 16:42:09 -0300

    feat: add new route to edit notes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
 import NotesPage from './pages/Notes'
+import EditNotes from './pages/EditNotes'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' exact element={ <Home />} />
         <Route path='/notes' exact element={ <NotesPage />} />
+        <Route path='/edit/:id' element={ <EditNotes />} />
       </Routes>
     </Router>
   );
