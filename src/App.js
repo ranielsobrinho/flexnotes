fdad6b3f183fa0a9340ede149414bfd9fd2a3416
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-03 10:44:42 -0300

    feat: add new route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Notes from './pages/Notes'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' exact element={ <Home />} />
+        <Route path='/notes' exact element={ <Notes />} />
       </Routes>
     </Router>
   );
