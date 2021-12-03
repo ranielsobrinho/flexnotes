617e75f6f92a6267fb849e4ece70426d50d7d503
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-03 16:44:34 -0300

    feat: displaying data of api and shows a loading svg while not loads

--- src/components/Notes/index.jsx
+++ src/components/Notes/index.jsx
@@ -1,26 +1,43 @@
 import React, { useState, useEffect } from 'react'
+import { Card, Button } from 'antd'
+import { EditOutlined } from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom'
 
 import Api from '../../services/Api'
+import Loading from '../Loading'
+import './index.css'
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
 
   useEffect(() => {
     Api.get('/notes')
       .then((response) => {
         setNotes(response.data.data)
+        setLoading(true)
       })
       .catch(err => console.error(err))
   }, [notes.id])
 
+  function edit(id){
+    navigate(`/edit/${id}`)
+  }
+
   return (
-    <div>
+    <div className='noteContainer'>
+      {!loading && <Loading />}
       {notes
         .map((note) => {
           return(
-            <div key={note.id}>
-              <p>{note.content}</p>
-              <p>{note.userId.username}</p>
+            <div key={note.id} className='card'>
+              <Card title={note.userId.username}
+               className='cardContent'
+               extra={<Button type='link' onClick={() => {edit(note.id)}}><EditOutlined /></Button>}
+              >
+                <p className='card-name'>{note.content}</p>
+              </Card>
             </div>
           )
         })
