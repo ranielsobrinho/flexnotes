0023bcc030e9ce40a5edfff7fbd9ba36509a705a
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-04 13:43:42 -0300

    feat: add form and buttons to edit note

--- src/pages/EditNotes/index.jsx
+++ src/pages/EditNotes/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'antd'
+// import Api from '../../services/Api'
+import './index.css'
 
 export default function EditNotes() {
+  const { register, handleSubmit } = useForm()
+  const navigate = useNavigate()
+
+  const submit = data => {
+    console.log(data)
+  }
+
+  function goBack(){
+    navigate('/notes')
+  }
+
   return (
-    <div>
-      <p>Edit your note here</p>
+    <div className='editNote'>
+      <h2>Edit your note here</h2>
+      <form onSubmit={handleSubmit(submit)}>
+        <input type='text' name='note' {...register('note')} />
+        <div className="editButtons">
+          <Button type='primary' htmlType='submit'>Editar</Button>
+          <Button type='default' onClick={() => {goBack()}}>Cancelar</Button>
+        </div>
+      </form>
     </div>
   )
 }
