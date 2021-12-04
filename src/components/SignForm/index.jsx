aa47e5286666dba0531f6855a5130d0d4ffc5500
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-04 17:54:48 -0300

    feat: showing error to the user

--- src/components/SignForm/index.jsx
+++ src/components/SignForm/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { Button } from 'antd'
@@ -7,6 +7,7 @@ import './index.css'
 
 export default function SignForm() {
   const { register, handleSubmit } = useForm()
+  const [ error, setError ] = useState('')
   const navigate = useNavigate()
 
   const submit = data => {
@@ -16,15 +17,13 @@ export default function SignForm() {
         navigate('/notes')
       })
       .catch((err) => {
-        // if(err.status === 401){
-        //   console.error('Senha inválida.')
-        // }
-        console.error('Senha ou usuário inválidos. Tente outra vez ou cadastre-se.')
+        setError('Senha ou usuário inválidos. Tente outra vez ou cadastre-se.')
       })
   }
   return (
     <div className='form'>
       <h2>Entrar</h2>
+      {error && <p className='error'>{error}</p>}
       <form onSubmit={handleSubmit(submit)}>
         <input type='text' name='username' {...register('username')} placeholder='Username'/>
 
