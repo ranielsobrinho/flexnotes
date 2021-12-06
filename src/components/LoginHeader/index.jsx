7413b2aa5422df5ecd981dc3928b0b33a97c9477
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-06 17:04:20 -0300

    feat: add login header

---
+++ src/components/LoginHeader/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Layout, Button, Modal, message } from 'antd'
+import { LogoutOutlined, PlusCircleOutlined } from '@ant-design/icons'
+import { BookOutlined } from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom'
+import NoteForm from '../NoteForm'
+import './index.css'
+
+
+export default function HeaderComponent() {
+  const { Header } = Layout
+  const navigate = useNavigate()
+
+  function info() {
+    Modal.info({
+      title: 'Clique no OK para fechar',
+      content: (
+        <div>
+          <NoteForm />
+        </div>
+      ),
+      onOk() {
+        message.info('Dados enviados.')
+      },
+    });
+  }
+
+  function getOut(){
+    sessionStorage.removeItem('token')
+    navigate('/')
+  }
+  return (
+    <div>
+      <Header>
+        <p className="headerTitle">FlexNotes<BookOutlined /></p>
+        <div className='headerButtons'>
+          <Button onClick={info} type='primary'><PlusCircleOutlined />Add Note</Button>
+          <Button type='link' onClick={() => {getOut()}}><LogoutOutlined />Sair</Button>
+        </div>
+      </Header>
+    </div>
+  )
+}
