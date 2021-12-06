83992a3f3c652c8d874edc3b4ffcbdfa921ab9b7
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-06 16:57:41 -0300

    style: add tabs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ import './index.css'
 export default function Home() {
   const [login, setLogin] = useState(true)
 
-  function toggleLogin(){
+  function toggleLogin() {
     setLogin(!login)
   }
 
@@ -17,20 +17,20 @@ export default function Home() {
     <div className='homeContainer'>
       <HeaderComponent />
       <div className="content">
-        { login ?
+        {login ?
           <>
-            <SignForm/>
+            <SignForm />
             <div className='toggle'>
               <p>Não tem uma conta?</p>
-              <Button type='link' onClick={() => {toggleLogin()}}>Faça a sua agora.</Button>
+              <Button type='link' onClick={() => { toggleLogin() }}>Faça a sua agora.</Button>
             </div>
           </>
-        :
+          :
           <>
             <RegisterForm />
             <div className='toggle'>
               <p>Já tem conta?</p>
-              <Button type='link' onClick={() => {toggleLogin()}}>Faça seu login.</Button>
+              <Button type='link' onClick={() => { toggleLogin() }}>Faça seu login.</Button>
             </div>
           </>
         }
