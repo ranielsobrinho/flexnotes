c6b16d3b695ac445af7547b3887feb50e44cce16
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-07 00:17:59 -0300

    feat: add a tab pannel

--- src/pages/NotesPage/index.jsx
+++ src/pages/NotesPage/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { Tabs } from 'antd'
 
 import LoginHeader from '../../components/LoginHeader'
 import Notes from '../../components/Notes'
+import UserNotes from '../../components/UserNotes'
 import './index.css'
 
 export default function NotesPage() {
+  const { TabPane } = Tabs
+
   return (
     <div>
       <LoginHeader />
-      <Notes />
+      <div className='tabs'>
+        <Tabs defaultActiveKey='1'>
+          <TabPane tab='Todas as anotações' key='1'>
+            <Notes />
+          </TabPane>
+          <TabPane tab='Suas anotações' key='2'>
+            <UserNotes />
+          </TabPane>
+        </Tabs>
+      </div>
     </div>
   )
 }
